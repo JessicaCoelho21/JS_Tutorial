fb54c56c42b6a1d8cef0ae1e7747036a4d7bd0ec
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-02 16:11:27 +0000

    Exercício 3 melhorado

--- w3resource_exercises/ex3.js
+++ w3resource_exercises/ex3.js
@@ -4,5 +4,27 @@ let day = date.getDay();
 let month = date.getMonth();
 let year = date.getFullYear();
 
-console.log((month + 1) + "-" + day + "-" + year + ", " + (month + 1) + "/" + day + "/" + year);
-console.log(day + "-" + (month + 1) + "-" + year + ", " + day + "/" + (month + 1) + "/" + year);
+if (month >= 9 && day >= 10) {
+    console.log((month + 1) + "-" + day + "-" + year);
+    console.log((month + 1) + "/" + day + "/" + year);
+    console.log(day + "-" + (month + 1) + "-" + year);
+    console.log(day + "/" + (month + 1) + "/" + year);
+} else if (month <= 9 && day >= 10) {
+    console.log("0" + (month + 1) + "-" + day + "-" + year);
+    console.log("0" + (month + 1) + "/" + day + "/" + year);
+    console.log(day + "-" + "0" + (month + 1) + "-" + year);
+    console.log(day + "/" + "0" + (month + 1) + "/" + year);
+} else if (month <= 9 && day <= 10) {
+    console.log("0" + (month + 1) + "-" + "0" + day + "-" + year);
+    console.log("0" + (month + 1) + "/" + "0" + day + "/" + year);
+    console.log("0" + day + "-" + "0" + (month + 1) + "-" + year);
+    console.log("0" + day + "/" + "0" + (month + 1) + "/" + year);
+} else if (month >= 9 && day <= 10) {
+    console.log((month + 1) + "-" + "0" + day + "-" + year);
+    console.log((month + 1) + "/" + "0" + day + "/" + year);
+    console.log("0" + day + "-" + (month + 1) + "-" + year);
+    console.log("0" + day + "/" + (month + 1) + "/" + year);
+}
+
+
+
