e0523030213341ca67a1eb182ed80b36230e40a9
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-02 17:45:28 +0000

    Exercise 7

---
+++ w3resource_exercises/ex7.js
@@ -0,0 +1,8 @@
+for (let i = 2014; i <= 2050; i++) {
+    //0 -> janeiro, 1 -> dia 1
+    let date = new Date(i, 0, 1);
+
+    if (date.getDay() === 0) {
+        console.log("1st Janueary is being a Sunday in " + i)
+    }
+}
