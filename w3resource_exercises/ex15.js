dcd128aed8621e49afe926103cc01592f4aca637
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-03 16:06:18 +0000

    Exercise 15

---
+++ w3resource_exercises/ex15.js
@@ -0,0 +1,10 @@
+function difference(number) {
+    if(number > 13) {
+        console.log((number - 13) * 2)
+    } else if (number <= 13) {
+        console.log(13 - number)
+    }
+}
+
+difference(10);
+difference(20);
