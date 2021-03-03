9a0b54c73f2499f16b620dfbb44b8e0b79264b04
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-03 16:19:36 +0000

    Exercise 18

---
+++ w3resource_exercises/ex18.js
@@ -0,0 +1,9 @@
+function number50(number1, number2) {
+    console.log(number1 === 50 || number2 === 50 || number1 + number2 === 50)
+}
+
+number50(10, 20)
+number50(50, 50)
+number50(20, 30)
+number50(50, 30)
+number50(30, 50)
