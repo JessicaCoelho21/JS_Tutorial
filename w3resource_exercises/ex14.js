20381859c2d7d8f9d4ea527c58a0db90ca5cba78
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-03 15:51:35 +0000

    Exercise 14

---
+++ w3resource_exercises/ex14.js
@@ -0,0 +1,8 @@
+let filename = "ex14.js"
+console.log(filename.split('.').pop());
+
+filename = "ex14.html"
+console.log(filename.split('.').pop());
+
+filename = "ex14.css"
+console.log(filename.split('.').pop());
