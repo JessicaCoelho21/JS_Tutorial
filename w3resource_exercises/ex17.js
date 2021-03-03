d7328c19f52d128416c54703a61703ed885005ed
JessicaCoelho21 <jscoelho21@gmail.com>
2021-03-03 16:15:09 +0000

    Exercise 17

---
+++ w3resource_exercises/ex17.js
@@ -0,0 +1,10 @@
+function absoluteDifference(number) {
+    if(number <= 19) {
+        console.log(19 - number)
+    } else {
+        console.log((number - 19) * 3)
+    }
+}
+
+absoluteDifference(30)
+absoluteDifference(10)
